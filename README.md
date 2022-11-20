065034dfda6d2f15811d20f787bd0b8edc0e103f
gottem87 <78823833+gottem87@users.noreply.github.com>
2022-11-19 20:45:18 -0600

    C

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Clickertycoon1
-A clicking tycoon. 
+A clicking tycoon. Click the [object] to get points. Use points to get upgrades. use upgrades to become the **most** powerfull player. 
+
+##WARNING!!
+###This game does not support save and load. Please visit a newer version to save and load. 
---
+++ index.js
@@ -0,0 +1,16 @@
+let points = 0;
+let cpc = 1;
+let cps = 1;
+
+document.getElementById("click").onclick = increase();
+
+function increase () {
+    points += cpc;
+    document.getElementById("clicks").innerHTML = points;
+}
+function clickTime () {
+    setInterval( cpsPoints, () => {
+	points += cps;
+    document.getElementById("clicks").innerHTML = points;
+}, 1000);
+}
